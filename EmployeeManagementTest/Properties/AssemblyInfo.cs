3322bc453f80fd8f5dcc174f043b3294c0bd6124
Ketha Charan <charan.ketha1747@gmail.com>
2021-06-09 12:10:18 -0700

    [Charan] Added: AdoProject

---
+++ EmployeeManagementTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EmployeeManagementTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EmployeeManagementTest")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1955c978-3a1e-4575-a466-6b37eba78b6f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ EmployeeManagementTest/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using EmployeeManagement;
+using EmployeeManagement.Model.SalaryModel;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace EmployeeManagementTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void GivenSalaryDataAbleToUpdateSalaryDetails()
+        {
+            Salary salary = new Salary();
+            SalaryUpdateModel updateModel = new SalaryUpdateModel()
+            {
+                SalaryId = 4,
+                Month = "Jan",
+                EmployeeSalary = 1300,
+                EmployeeId = 2
+            };
+
+            int EmpSalary = salary.UpdateEmployeeSalary(updateModel);
+
+            Assert.AreEqual(updateModel.EmployeeSalary, EmpSalary);
+        }
+    }
+
+}
